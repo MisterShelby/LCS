dda186eb919c0f149d1397d91c62f568d491f31b
Иван Соломка <solvan007@gmail.com>
2019-12-10 14:08:29 +0200

    Add project files.

---
+++ LCS/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LCS
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string[] inputStrings = Console.ReadLine().Split(' ');
+
+			var longestCommonSubsequence = LCS.LCS.Find(inputStrings[0], inputStrings[1]);
+
+			Console.WriteLine(longestCommonSubsequence.Length);
+		}
+	}
+}
